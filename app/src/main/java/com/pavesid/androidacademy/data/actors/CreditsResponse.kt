aae09791247186ac2d60bc23f9c14ae59264aac3
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-01-11 13:35:47 +0300

    fix: add serialName to data.

--- app/src/main/java/com/pavesid/androidacademy/data/actors/CreditsResponse.kt
+++ app/src/main/java/com/pavesid/androidacademy/data/actors/CreditsResponse.kt
@@ -1,10 +1,14 @@
 package com.pavesid.androidacademy.data.actors
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CreditsResponse(
+    @SerialName("cast")
     val cast: List<Cast>,
+    @SerialName("crew")
     val crew: List<Crew>,
+    @SerialName("id")
     val id: Int
 )

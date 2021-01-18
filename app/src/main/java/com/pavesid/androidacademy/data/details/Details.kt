b57d6ac0339430289ce882439a99fa36b67958c3
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-01-18 13:47:18 +0300

    refactor: remove SerialName.

--- app/src/main/java/com/pavesid/androidacademy/data/details/Details.kt
+++ app/src/main/java/com/pavesid/androidacademy/data/details/Details.kt
@@ -2,13 +2,9 @@ package com.pavesid.androidacademy.data.details
 
 import com.pavesid.androidacademy.data.actors.Cast
 import com.pavesid.androidacademy.data.actors.Crew
-import kotlinx.serialization.SerialName
 
 data class Details(
-    @SerialName("detailsResponse")
     val detailsResponse: DetailsResponse,
-    @SerialName("cast")
     val cast: List<Cast>,
-    @SerialName("crew")
     val crew: List<Crew>
 )

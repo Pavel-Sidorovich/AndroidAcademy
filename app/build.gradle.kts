cce7807099e0497d233c5b7a309be5f83bb28afa
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-02-11 08:02:45 +0300

    refactor: split build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,8 @@ plugins {
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
     id("com.github.ben-manes.versions")
-    jacoco
+    id("jacoco_custom")
+    id("ktlint")
 }
 
 android {
@@ -76,124 +77,4 @@ dependencies {
 
 kapt {
     correctErrorTypes = true
-}
-
-fun Project.getKtlintConfiguration(): Configuration {
-    return configurations.findByName("ktlint") ?: configurations.create("ktlint") {
-        val dependency = project.dependencies.create(AppDependencies.ktlint)
-        dependencies.add(dependency)
-    }
-}
-
-tasks.withType<Test> {
-    configure<JacocoTaskExtension> {
-        isIncludeNoLocationClasses = true
-    }
-}
-
-private val classDirectoriesTree = fileTree(project.buildDir) {
-    include(
-        "**/classes/**/main/**",
-        "**/intermediates/classes/debug/**",
-        "**/intermediates/javac/debug/*/classes/**",
-        "**/tmp/kotlin-classes/debug/**"
-    )
-
-    exclude(
-        "**/R.class",
-        "**/R\$*.class",
-        "**/BuildConfig.*",
-        "**/Manifest*.*",
-        "**/*Test*.*",
-        "android/**/*.*",
-        "**/*\$Lambda$*.*", // Jacoco can not handle several "$" in class name.
-        "**/*\$inlined$*.*"
-    )
-}
-
-private val sourceDirectoriesTree = fileTree("${project.buildDir}") {
-    include(
-        "src/main/java/**",
-        "src/main/kotlin/**",
-        "src/debug/java/**",
-        "src/debug/kotlin/**"
-    )
-}
-
-private val executionDataTree = fileTree(project.buildDir) {
-    include(
-        "outputs/code_coverage/**/*.ec",
-        "jacoco/jacocoTestReportDebug.exec",
-        "jacoco/testDebugUnitTest.exec",
-        "jacoco/test.exec"
-    )
-}
-
-fun JacocoReportsContainer.reports() {
-    html.isEnabled = true
-    html.destination = file("${buildDir}/reports/jacoco/jacocoTestReport/html")
-}
-
-fun JacocoCoverageVerification.setDirectories() {
-    sourceDirectories.setFrom(sourceDirectoriesTree)
-    classDirectories.setFrom(classDirectoriesTree)
-    executionData.setFrom(executionDataTree)
-}
-
-fun JacocoReport.setDirectories() {
-    sourceDirectories.setFrom(sourceDirectoriesTree)
-    classDirectories.setFrom(classDirectoriesTree)
-    executionData.setFrom(executionDataTree)
-}
-
-val jacocoAndroidTestReport by tasks.creating(JacocoReport::class) {
-    group = "Publishing"
-    description = "Code coverage report for both Android and Unit tests."
-    dependsOn("testDebugUnitTest", "createDebugCoverageReport")
-    reports {
-        reports()
-    }
-    setDirectories()
-}
-
-val jacocoAndroidCoverageVerification by tasks.creating(JacocoCoverageVerification::class) {
-    group = "Publishing"
-    description = "Code coverage verification for Android both Android and Unit tests."
-    dependsOn("testDebugUnitTest", "createDebugCoverageReport")
-    violationRules {
-        rule {
-            limit {
-                counter = "INSTRUCTIONAL"
-                value = "COVEREDRATIO"
-                minimum = "0.5".toBigDecimal()
-            }
-        }
-    }
-    setDirectories()
-}
-
-val outputDirJacoco = "${project.buildDir}/reports/jacoco"
-val outputDir = "${project.buildDir}/reports/ktlint/"
-val inputFiles = project.fileTree(mapOf("dir" to "src", "include" to "**/*.kt"))
-
-val ktlintDebugCheck by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    group = "Publishing"
-    description = "Check Kotlin code style."
-    classpath = getKtlintConfiguration()
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("src/**/*.kt")
-}
-
-val ktlintFormat by tasks.creating(JavaExec::class) {
-    inputs.files(inputFiles)
-    outputs.dir(outputDir)
-
-    group = "Publishing"
-    description = "Fix Kotlin code style deviations."
-    classpath = getKtlintConfiguration()
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("-F", "src/**/*.kt")
 }

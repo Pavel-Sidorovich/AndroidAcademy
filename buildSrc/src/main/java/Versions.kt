b6500439677139bf11e922dc511f392d43f2358d
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-02-11 08:17:07 +0300

    refactor: deleted comments

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,28 +4,28 @@ object Versions {
     const val kotlin_version = "1.4.30"
 
     // Libs
-    const val coil_version = "1.1.1" //1.0.0
+    const val coil_version = "1.1.1"
     const val coil_transformation_version = "1.0.0"
     const val rating_bar_version = "1.4.0"
     const val coroutines_version = "1.3.9"
-    const val lifecycle_version = "2.3.0-rc01" //2.2.0
+    const val lifecycle_version = "2.3.0-rc01"
     const val fragment_ktx_version = "1.2.5"
     const val preference_ktx_version = "1.1.1"
     const val leak_canary_version = "2.6"
-    const val core_ktx_version = "1.5.0-beta01" //1.5.0-alpha05
+    const val core_ktx_version = "1.5.0-beta01"
     const val appcompat_version = "1.2.0"
-    const val material_version = "1.3.0" //1.2.1
-    const val swipe_refresh_layout = "1.2.0-alpha01" //1.1.0
-    const val constraint_layout_version = "2.1.0-alpha2" //2.0.4
+    const val material_version = "1.3.0"
+    const val swipe_refresh_layout = "1.2.0-alpha01"
+    const val constraint_layout_version = "2.1.0-alpha2"
     const val serialization_version = "1.0.1"
     const val timber_version = "4.7.1"
-    const val hilt_version = "2.32-alpha" //2.31.2-alpha
+    const val hilt_version = "2.32-alpha"
     const val hilt_compiler_version = "1.0.0-alpha03"
     const val logging_version = "4.9.0"
     const val retrofit_version = "2.9.0"
     const val serialization_converter_version = "0.8.0"
-    const val lottie_version = "3.6.0" //3.5.0
-    const val room_version = "2.3.0-beta01" //2.2.6
+    const val lottie_version = "3.6.0"
+    const val room_version = "2.3.0-beta01"
     const val bang_version = "1.2.2"
     const val work_manager_version = "2.5.0"
     const val kt_lint_version = "0.40.0"
